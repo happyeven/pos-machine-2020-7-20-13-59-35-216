42ad7c6ec8d38e14ea7a55f961a87faecefc7630
happyeven <1361840595@qq.com>
2020-07-26 23:40:27 +0800

    [Pos v0.2] [Dong] refactor:extract a generateCartItemsReceipt method and rename some variables

--- main.js
+++ main.js
@@ -1,128 +1,117 @@
 
-function getdata(){
-    var data = [
-        {
-           barcode: 'ITEM000000',
-           name: 'Coca-Cola',
-           price: 3
-         },
-         {
-           barcode: 'ITEM000001',
-           name: 'Sprite',
-           price: 3
-         },
-         {
-           barcode: 'ITEM000002',
-           name: 'Apple',
-           price: 5
-         },
-         {
-           barcode: 'ITEM000003',
-           name: 'Litchi',
-           price: 15
-         },
-         {
-           barcode: 'ITEM000004',
-           name: 'Battery',
-           price: 2
-         },
-         {
-           barcode: 'ITEM000005',
-           name: 'Instant Noodles',
-           price: 4
-         }
-     ]
-     return data
-}
-function createReceipt(barCodes){
-    var itemArr = transIntoItemArr(barCodes);
-    var itemInfoArr = replenishItemInfo(itemArr);
-    var itemInfoDetailsArr = calculateItemSubtotalPrice(itemInfoArr);
-    var totalPrice = calculateItemsTotalPrice(itemInfoDetailsArr);
-    return generateRecerpt(itemInfoDetailsArr,totalPrice)
-}
-function transIntoItemArr(barCodes){
-    var barCodeAmountMap = getBarCodeAmountMap(barCodes)
-    var itemArr = []
-    for(let key in barCodeAmountMap){
-        var item = {}
-        item.barcode = key
-        item.Quantity = barCodeAmountMap[key]
-        itemArr.push(item)
+function getdata() {
+  let data = [
+    {
+      barcode: 'ITEM000000',
+      name: 'Coca-Cola',
+      price: 3
+    },
+    {
+      barcode: 'ITEM000001',
+      name: 'Sprite',
+      price: 3
+    },
+    {
+      barcode: 'ITEM000002',
+      name: 'Apple',
+      price: 5
+    },
+    {
+      barcode: 'ITEM000003',
+      name: 'Litchi',
+      price: 15
+    },
+    {
+      barcode: 'ITEM000004',
+      name: 'Battery',
+      price: 2
+    },
+    {
+      barcode: 'ITEM000005',
+      name: 'Instant Noodles',
+      price: 4
     }
-    return itemArr
+  ]
+  return data
 }
-function replenishItemInfo(itemArr){
-    var database = getdata()
-    for(let index in itemArr){
-        var item = searchInfoFromDatabase(itemArr[index].barcode,database)
-        itemArr[index].unitPrice = item.price
-        itemArr[index].Name = item.name
+function createReceipt(barCodes) {
+  let cartItems = transIntoCartItem(barCodes);
+  let cartItemsDetails = replenishItemInfo(cartItems);
+  let cartItemsDetailsWithTotalPrice = calculateItemSubtotalPrice(cartItemsDetails);
+  let totalPrice = calculateItemsTotalPrice(cartItemsDetailsWithTotalPrice);
+  return generateRecerpt(cartItemsDetailsWithTotalPrice, totalPrice)
+}
+function transIntoCartItem(barCodes) {
+  let barCodeAmountMap = getBarCodeAmountMap(barCodes)
+  let cartItems = []
+  for (let key in barCodeAmountMap) {
+    let item = {}
+    item.barcode = key
+    item.Quantity = barCodeAmountMap[key]
+    cartItems.push(item)
+  }
+  return cartItems
+}
+function replenishItemInfo(cartItems) {
+  let database = getdata()
+  for (let index in cartItems) {
+    let item = searchInfoFromDatabase(cartItems[index].barcode, database)
+    cartItems[index].unitPrice = item.price
+    cartItems[index].Name = item.name
+  }
+  return cartItems
+}
+function calculateItemSubtotalPrice(cartItemsDetails) {
+  for (let index in cartItemsDetails) {
+    cartItemsDetails[index].Subtotal = cartItemsDetails[index].unitPrice * cartItemsDetails[index].Quantity
+  }
+  return cartItemsDetails
+}
+function calculateItemsTotalPrice(cartItemsDetailsWithTotalPrice) {
+  let totalPrice = 0;
+  for (let index in cartItemsDetailsWithTotalPrice) {
+    totalPrice += cartItemsDetailsWithTotalPrice[index].Subtotal
+  }
+  return totalPrice
+}
+function generateRecerpt(cartItemsDetailsWithTotalPrice, totlaPrice) {//todo
+  let receipt = "\n" + "***<store earning no money>Receipt ***\n"
+  receipt += generateCartItemsReceipt(cartItemsDetailsWithTotalPrice)
+  receipt += "----------------------\nTotal: " + totlaPrice + " (yuan)\n**********************"
+  return receipt
+}
+function generateCartItemsReceipt(cartItemsDetailsWithTotalPrice) {
+  let receipt = ""
+  for (let index in cartItemsDetailsWithTotalPrice) {
+    let itemInfoDetail = cartItemsDetailsWithTotalPrice[index]
+    receipt = receipt + "Name: " + itemInfoDetail.Name + ", Quantity: " + itemInfoDetail.Quantity + ", Unit price: " + itemInfoDetail.unitPrice
+      + " (yuan), Subtotal: " + itemInfoDetail.Subtotal + " (yuan)" + "\n"
+  }
+  return receipt
+}
+function getBarCodeAmountMap(barCodes) {
+  let countedNames = barCodes.reduce(function (allNames, name) {
+    if (name in allNames) {
+      allNames[name]++;
     }
-    return itemArr
-}
-function calculateItemSubtotalPrice(itemInfoArr){
-    for(var index in itemInfoArr){
-        itemInfoArr[index].Subtotal = itemInfoArr[index].unitPrice * itemInfoArr[index].Quantity
+    else {
+      allNames[name] = 1;
     }
-    return itemInfoArr
+    return allNames;
+  }, {});
+  return countedNames
 }
-function calculateItemsTotalPrice(itemInfoDetailsArr){
-    var totalPrice = 0;
-    for(var index in itemInfoDetailsArr){
-        totalPrice += itemInfoDetailsArr[index].Subtotal
+function searchInfoFromDatabase(barcode, database) {
+  for (let index in database) {
+    if (database[index].barcode == barcode) {
+      return database[index]
     }
-    return totalPrice
+  }
 }
-function generateRecerpt(itemInfoDetailsArr,totlaPrice){
-    var receipt = "\n" + "***<store earning no money>Receipt ***" + "\n"
-    for(var index in itemInfoDetailsArr){
-        var itemInfoDetail = itemInfoDetailsArr[index]
-        receipt = receipt + "Name: " + itemInfoDetail.Name + ", Quantity: " + itemInfoDetail.Quantity + ", Unit price: " + itemInfoDetail.unitPrice 
-            + " (yuan), Subtotal: " + itemInfoDetail.Subtotal + " (yuan)" + "\n"
-    }
-    receipt += "----------------------"
-    receipt += "\n"
-    receipt = receipt + "Total: " + totlaPrice +" (yuan)" + "\n"
-    receipt += "**********************"
-    console.log(receipt)
-    return receipt
+function printReceipt(barCodes) {
+  console.log(createReceipt(barCodes))
 }
-function getBarCodeAmountMap(barCodes){
-    var countedNames = barCodes.reduce(function (allNames, name) { 
-        if (name in allNames) {
-          allNames[name]++;
-        }
-        else {
-          allNames[name] = 1;
-        }
-        return allNames;
-      }, {});
-      return countedNames
-}
-function searchInfoFromDatabase(barcode,database){
-    for(let index in database){
-        if(database[index].barcode == barcode){
-            return database[index]
-        }
-    }
-}
-function printReceipt(){
-	var barCodes = [
-	  'ITEM000000',
-	  'ITEM000000',
-	  'ITEM000000',
-	  'ITEM000000',
-	  'ITEM000000',
-	  'ITEM000001',
-	  'ITEM000001',
-	  'ITEM000004'
-	]
-createReceipt(barCodes)
-}
-
-
 
 module.exports = {
-    printReceipt
+  printReceipt
 };
